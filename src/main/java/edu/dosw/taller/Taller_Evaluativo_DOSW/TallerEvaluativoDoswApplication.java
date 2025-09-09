d41b829996f9c37be7daeea68c6637bde2b5b87b
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-09-09 14:29:20 -0500

    Project setup

---
+++ src/main/java/edu/dosw/taller/Taller_Evaluativo_DOSW/TallerEvaluativoDoswApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.taller.Taller_Evaluativo_DOSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerEvaluativoDoswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerEvaluativoDoswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/taller/Taller_Evaluativo_DOSW/TallerEvaluativoDoswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.taller.Taller_Evaluativo_DOSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerEvaluativoDoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
