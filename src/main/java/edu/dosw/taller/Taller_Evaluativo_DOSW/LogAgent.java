64a45d2444d7563543fea27cb47221dd2b48ec73
Juan Pablo Contreras <juan.contreras-p@mail.escuelaing.edu.co>
2025-09-11 18:59:11 -0500

    diagrams upload

--- src/main/java/edu/dosw/taller/Taller_Evaluativo_DOSW/LogAgent.java
+++
@@ -1,8 +0,0 @@
-package edu.dosw.taller.Taller_Evaluativo_DOSW;
-
-public class LogAgent implements StockObserver{
-    @Override
-    public void writeUpdate(Product product){
-        System.out.println(product.getName() + ": " + product.getStock() + " unidades");
-    }
-}
